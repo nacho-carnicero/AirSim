13b488ef36b695e3740e82e0673fc92a83f8cd75
nacho <ignacio.carnicero@sterblue.com>
2017-06-12 18:33:16 +0200

    Debug message for knowing when a node is sending a heartbeat message

--- MavLinkCom/src/impl/MavLinkNodeImpl.cpp
+++ MavLinkCom/src/impl/MavLinkNodeImpl.cpp
@@ -50,15 +50,16 @@ void MavLinkNodeImpl::sendHeartbeat()
     while (heartbeat_running_) {
         MavLinkHeartbeat heartbeat;
         // send a heart beat so that the remote node knows we are still alive
-        // (otherwise drone will trigger a failsafe operation).	
+        // (otherwise drone will trigger a failsafe operation).
         heartbeat.autopilot = static_cast<uint8_t>(MAV_AUTOPILOT::MAV_AUTOPILOT_INVALID);
         heartbeat.type = static_cast<uint8_t>(MAV_TYPE::MAV_TYPE_GCS);
         heartbeat.mavlink_version = 3;
         heartbeat.base_mode = 0; // ignored by PX4
         heartbeat.custom_mode = 0; // ignored by PX4
         heartbeat.system_status = 0; // ignored by PX4
+				Utils::logMessage("Sending heartbeat message");
         sendMessage(heartbeat);
-        
+
         std::this_thread::sleep_for(std::chrono::milliseconds(heartbeatMilliseconds));
     }
 }
@@ -68,7 +69,7 @@ void MavLinkNodeImpl::handleMessage(std::shared_ptr<MavLinkConnection> connectio
 {
     unused(connection);
     unused(msg);
-	// this is for the subclasses to play with.  We put nothing here so we are not dependent on the 
+	// this is for the subclasses to play with.  We put nothing here so we are not dependent on the
 	// subclasses remembering to call this base implementation.
 }
 
@@ -149,7 +150,7 @@ AsyncResult<MavLinkHeartbeat>  MavLinkNodeImpl::waitForHeartbeat()
 		}
 	});
 	result.setState(subscription);
-	
+
 	return result;
 }
 
@@ -302,7 +303,7 @@ std::vector<MavLinkParameter> MavLinkNodeImpl::getParamList()
 		}
 	});
 
-	//MAVLINK_MSG_ID_PARAM_REQUEST_LIST	
+	//MAVLINK_MSG_ID_PARAM_REQUEST_LIST
 	MavLinkParamRequestList cmd;
 	cmd.target_system = getTargetSystemId();
 	cmd.target_component = getTargetComponentId();
@@ -323,7 +324,7 @@ std::vector<MavLinkParameter> MavLinkNodeImpl::getParamList()
 	std::vector<size_t> missing;
 
 	for (size_t i = 0; i < paramCount; i++)
-	{		
+	{
 		// nested loop is inefficient, but it is needed because UDP also doesn't guarantee in-order delivery
 		bool found = false;
 		for (auto iter = result.begin(), end = result.end(); iter != end; iter++)
